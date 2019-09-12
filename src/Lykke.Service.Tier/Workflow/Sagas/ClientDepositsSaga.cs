b961d693b75ef11e55407d79477b143fa7a6f384
Dmitry Samodov <samodov.dmitry@gmail.com>
2019-09-12 23:44:21 +0300

    LWDEV-10579: fix for push template

--- src/Lykke.Service.Tier/Workflow/Sagas/ClientDepositsSaga.cs
+++ src/Lykke.Service.Tier/Workflow/Sagas/ClientDepositsSaga.cs
@@ -83,7 +83,7 @@ namespace Lykke.Service.Tier.Workflow.Sagas
                         new
                         {
                             CurrentAmount = checkAmount,
-                            Limit = currentLimitSettings,
+                            Limit = currentLimitSettings.MaxLimit.Value,
                             Percent = Math.Round(checkAmount / currentLimitSettings.MaxLimit.Value * 100),
                             FullName = pd.FullName,
                             Asset = evt.Asset
