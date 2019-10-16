a84e60f6c2c177a18998a6094b04278e6ec10f9d
Dmitry Samodov <samodov.dmitry@gmail.com>
2019-10-16 18:48:27 +0300

    LWDEV-10587: changed method of getting tier info

---
+++ src/Lykke.Service.Tier.Domain/TierUpgradeRequest.cs
@@ -0,0 +1,12 @@
+using System;
+using Lykke.Service.ClientAccount.Client.Models;
+
+namespace Lykke.Service.Tier.Domain
+{
+    public class TierUpgradeRequest
+    {
+        public AccountTier Tier { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public string Status { get; set; }
+    }
+}
