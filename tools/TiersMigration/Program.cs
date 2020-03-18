0d91e50f2a57d42dcbc1fcd31e8565eace55711d
Dmitry Samodov <samodov.dmitry@gmail.com>
2020-03-18 19:52:05 +0300

    get information about KOR users

--- tools/TiersMigration/Program.cs
+++ tools/TiersMigration/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,13 +11,8 @@ using AzureStorage.Tables;
 using Lykke.Common.Log;
 using Lykke.Logs;
 using Lykke.Messages.Email;
-using Lykke.Messages.Email.MessageData;
 using Lykke.Service.ClientAccount.Client;
-using Lykke.Service.ClientAccount.Client.Models;
-using Lykke.Service.ClientAccount.Client.Models.Request.ClientAccount;
 using Lykke.Service.History.Client;
-using Lykke.Service.History.Contracts.Enums;
-using Lykke.Service.History.Contracts.History;
 using Lykke.Service.Kyc.Abstractions.Domain.Verification;
 using Lykke.Service.Kyc.Abstractions.Services;
 using Lykke.Service.Kyc.Client;
@@ -28,11 +21,8 @@ using Lykke.Service.PersonalData.Contract;
 using Lykke.Service.PersonalData.Settings;
 using Lykke.Service.RateCalculator.Client;
 using Lykke.Service.TemplateFormatter;
-using Lykke.Service.TemplateFormatter.Client;
 using Lykke.Service.Tier.AzureRepositories;
 using Lykke.Service.Tier.Client;
-using Lykke.Service.Tier.Client.Models;
-using Lykke.Service.Tier.Client.Models.Requests;
 using Lykke.Service.Tier.Client.Models.Responses;
 using Lykke.SettingsReader.ReloadingManager;
 using Microsoft.Extensions.Configuration;
@@ -47,46 +37,30 @@ namespace TiersMigration
                 .AddJsonFile("appsettings.json")
                 .Build();
 
-            ILogFactory logFactory = EmptyLogFactory.Instance;
-
             var settings = new AppSettings();
 
             config.Bind(settings);
 
             var container = BuildContainer(settings);
 
-            var kycStatusesStorage = AzureTableStorage<KycEntity>.Create(
-                ConstantReloadingManager.From(settings.KycStatusesConnString), "KycStatuses", logFactory);
-
-            Console.WriteLine("Gettings KYC Ok clients...");
             var sb = new StringBuilder();
-            sb.AppendLine("ClientId,Email,Country,CountryRisk,Tier,Limit,Deposits,ChangeTier,SetLimit,SendEmail,Comment");
+            sb.AppendLine("ClientId,Email,CountryPOA,Country risk,KYC status,currentTier,currentLimit,depositAmount,Comment");
 
-            // var ids = new List<string>()
-            // {
-            //     "3dbd99ad-449f-4a31-b0a5-e9702b8efc15",
-            //     "e7e16a0d-5582-4b0b-a61b-dd3474eb1c5c",
-            //     "93b40db0-f4bb-44ce-a338-5370f8e27296",
-            //     "ad51270f-7342-4aa0-b64c-f84dd9cbc989",
-            //     "81b8086e-b99c-427d-bab8-085abca9e5e2",
-            //     "528c0279-69b9-475a-947b-e484867ef89e"
-            // };
+            var clientAccountClient = container.Resolve<IClientAccountClient>();
+            string continuationToken = null;
+            bool done;
 
-            var webClient = new WebClient();
-            var template =
-                await webClient.DownloadStringTaskAsync(
-                    "https://lkefiles.blob.core.windows.net/mails/LykkeWallet/TierUpgradedTemplate.html");
-
-            //ProcessClientsAsync(ids, container, sb, template).GetAwaiter().GetResult();
-
-            await kycStatusesStorage.GetDataByChunksAsync("Ok", entities =>
+            do
             {
-                var items = entities.ToList();
+                var ids = await clientAccountClient.Clients.GetIdsAsync(continuationToken);
+                await ProcessClientsAsync(ids.Ids, container, sb);
 
-                ProcessClientsAsync(items.Select(x => x.ClientId), container, sb, template).GetAwaiter().GetResult();
-            });
+                done = string.IsNullOrEmpty(ids.ContinuationToken);
+                continuationToken = ids.ContinuationToken;
 
-            var filename = $"tiers-migration-emails-{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.csv";
+            } while (!done);
+
+            var filename = $"tiers-info-{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.csv";
             Console.WriteLine($"Saving results to {filename}...");
 
             using (var sw = new StreamWriter(filename))
@@ -97,14 +71,15 @@ namespace TiersMigration
             Console.WriteLine("Done!");
         }
 
-        private static async Task ProcessClientsAsync(IEnumerable<string> clientIds, IContainer container, StringBuilder sb, string template)
+        private static async Task ProcessClientsAsync(IEnumerable<string> clientIds, IContainer container, StringBuilder sb)
         {
             var personalDataService = container.Resolve<IPersonalDataService>();
             var tierClient = container.Resolve<ITierClient>();
-            var clientAccountClient = container.Resolve<IClientAccountClient>();
             var kycStatusService = container.Resolve<IKycStatusService>();
 
-            var personalDatas = (await personalDataService.GetAsync(clientIds)).ToList();
+            var personalDatas = (await personalDataService.GetAsync(clientIds))
+                .Where(x => x.CountryFromPOA == "KOR")
+                .ToList();
 
             Console.WriteLine($"Processing {personalDatas.Count} items");
             int index = 0;
@@ -117,225 +92,21 @@ namespace TiersMigration
                     Console.WriteLine($"({index} of {personalDatas.Count} chunk). Processing client = {pd.Id}");
                     var tierInfoTask = tierClient.Tiers.GetClientTierInfoAsync(pd.Id);
                     var countryRiskTask = tierClient.Countries.GetCountryRiskAsync(pd.CountryFromPOA);
-
-                    Task.WhenAll(countryRiskTask, tierInfoTask).GetAwaiter().GetResult();
+                    var kycStatusTask = kycStatusService.GetKycStatusAsync(pd.Id);
+                    await Task.WhenAll(countryRiskTask, tierInfoTask, kycStatusTask);
 
                     TierInfoResponse tierInfo = tierInfoTask.Result;
                     CountryRiskResponse countryRisk = countryRiskTask.Result;
+                    KycStatus kycStatus = kycStatusTask.Result;
 
-                    // if (tierInfo.CurrentTier.Tier != AccountTier.Beginner)
-                    //     continue;
-
-                    AccountTier tier;
-                    double limit;
-                    string comment = string.Empty;
-                    string changeTier = "-";
-                    string setLimit = "-";
-                    string sendEmail = "-";
-                    bool isRestricted = countryRisk.Risk == null;
-                    double totalDepositAmount = 0;
-                    (tier, limit, comment) = GetTierAndLimit(container, countryRisk.Risk, pd.Email.ToLowerInvariant());
-
-                    // try
-                    // {
-                    //     clientAccountClient.ClientAccount
-                    //         .ChangeAccountTierAsync(pd.Id, new AccountTierRequest {Tier = tier}).GetAwaiter()
-                    //         .GetResult();
-                    //     changeTier = "success";
-                    // }
-                    // catch(Exception ex)
-                    // {
-                    //     changeTier = ex.Message;
-                    // }
-
-                    // try
-                    // {
-                    //     tierClient.Limits.SetLimitAsync(new SetLimitRequest {ClientId = pd.Id, Limit = limit})
-                    //         .GetAwaiter().GetResult();
-                    //     setLimit = "success";
-                    // }
-                    // catch (Exception ex)
-                    // {
-                    //     setLimit = ex.Message;
-                    // }
-                    //
-                    // var totalDepositAmount = MigrateDepositsAsync(container, pd.Id).GetAwaiter().GetResult();
-                    //
-                    // if (totalDepositAmount > Convert.ToDecimal(limit) && !isRestricted)
-                    // {
-                    //     if (string.IsNullOrEmpty(comment))
-                    //         comment = "Deposited amount is bigger than limit!";
-                    //     else
-                    //         comment += "; Deposited amount is bigger than limit!";
-                    //
-                    //     try
-                    //     {
-                    //         kycStatusService.ChangeKycStatusAsync(pd.Id, KycStatus.NeedToFillData,
-                    //                 $"TiersMigration script - limit reached ({totalDepositAmount} of {limit} EUR)")
-                    //             .GetAwaiter().GetResult();
-                    //         comment += "; Kyc status changed to NeedToFillData";
-                    //     }
-                    //     catch (Exception ex)
-                    //     {
-                    //         comment += $"; Error changing kyc status to NeedToFillData: {ex.Message}";
-                    //     }
-                    // }
-                    //
-                    if (tierInfo.CurrentTier.MaxLimit > 0)
-                    {
-                        try
-                        {
-                            SendEmailAsync(container, template, tierInfo.NextTier?.Tier, tierInfo.CurrentTier.Tier, tierInfo.CurrentTier.MaxLimit, pd.Email).GetAwaiter().GetResult();
-                            sendEmail = "success";
-                        }
-                        catch (Exception ex)
-                        {
-                            sendEmail = ex.Message;
-                        }
-                    }
-
-                    sb.AppendLine($"{pd.Id},{pd.Email},{pd.CountryFromPOA},{countryRisk.Risk},{tier},{limit},{totalDepositAmount},{changeTier},{setLimit},{sendEmail},{comment}");
+                    sb.AppendLine($"{pd.Id},{pd.Email},{pd.CountryFromPOA ?? "-"},{countryRisk.Risk?.ToString() ?? "-"},{kycStatus},{tierInfo.CurrentTier.Tier},{tierInfo.CurrentTier.MaxLimit},{tierInfo.CurrentTier.Current},-");
                 }
                 catch (Exception ex)
                 {
-                    sb.AppendLine($"{pd.Id},{pd.Email},{pd.CountryFromPOA},-,-,-,-,-,-,-,{ex.Message}");
+                    sb.AppendLine($"{pd.Id},{pd.Email},{pd.CountryFromPOA},-,-,-,-,-,{ex.Message}");
                     Console.WriteLine($"ClientId = {pd.Id}: {ex.Message}");
                 }
             }
-        }
-
-        private static (AccountTier tier, double limit, string comment) GetTierAndLimit(IContainer container, RiskModel? countryRisk, string email)
-        {
-            var settings = container.Resolve<AppSettings>();
-
-            if (countryRisk == null)
-            {
-                return (AccountTier.ProIndividual, 0, "restricted country");
-            }
-
-            var isTier2 = settings.Tier2Emails.ContainsKey(email);
-
-            if (isTier2)
-            {
-                return (AccountTier.ProIndividual, settings.Tier2Emails[email], "limits according the list");
-            }
-
-            double limit = 0;
-            AccountTier tier = AccountTier.Beginner;
-
-            switch (countryRisk.Value)
-            {
-                case RiskModel.Low:
-                    limit = 15000;
-                    tier = AccountTier.Advanced;
-                    break;
-                case RiskModel.Mid:
-                    limit = 7500;
-                    tier = AccountTier.Advanced;
-                    break;
-                case RiskModel.High:
-                    limit = 2000;
-                    tier = AccountTier.ProIndividual;
-                    break;
-            }
-
-            return (tier, limit, string.Empty);
-        }
-
-        private static async Task SendEmailAsync(IContainer container, string template, AccountTier? nextTier, AccountTier tier, double limit, string email)
-        {
-            var sb = new StringBuilder();
-            var templateFormatter = container.Resolve<ITemplateFormatter>();
-            var emailSender = container.Resolve<IEmailSender>();
-
-            if (nextTier != null)
-            {
-                sb.AppendLine(
-                    $"If you wish to increase deposit limit, just <a href='https://wallet.lykke.com/profile/kyc'>upgrade to Pro Individual account</a>.");
-                sb.AppendLine("<br>Or use Lykke Wallet mobile app (More->Profile->Upgrade)");
-            }
-
-            var sw = new Stopwatch();
-            sw.Start();
-
-            string emailTemplate = template
-                .Replace("@[Tier]", tier.ToString())
-                .Replace("@[Amount]", $"{limit} EUR")
-                .Replace("@[UpgradeText]", sb.ToString())
-                .Replace("@[Year]", DateTime.UtcNow.Year.ToString());
-
-            var msgData = new PlainTextData
-            {
-                Sender = email,
-                Subject = "Account level upgraded",
-                Text = emailTemplate
-            };
-
-            await emailSender.SendEmailAsync(null, email, msgData);
-            sw.Stop();
-            Console.WriteLine($"email sent [{sw.ElapsedMilliseconds} msec. ]");
-        }
-
-        public static async Task<decimal> MigrateDepositsAsync(IContainer container, string clientId)
-        {
-            var historyClient = container.Resolve<IHistoryClient>();
-            var rateCalculatorClient = container.Resolve<IRateCalculatorClient>();
-            var depositsStorage = container.Resolve<INoSQLTableStorage<DepositOperationEntity>>();
-
-            var to = DateTime.UtcNow;
-            var from = to.AddMonths(-1);
-
-            var historyItems = (await historyClient.HistoryApi.GetHistoryByWalletAsync(Guid.Parse(clientId),
-                            new[] {HistoryType.CashIn}, limit: int.MaxValue,
-                            @from: from, to: to))
-                .Select(x => (CashinModel)x).ToList();
-
-            decimal result = 0;
-
-            var deposits = new List<DepositOperationEntity>();
-
-            foreach (var item in historyItems)
-            {
-                decimal amount;
-
-                if (item.AssetId == "EUR")
-                {
-                    amount = item.Volume;
-                }
-                else
-                {
-                    amount = (decimal)rateCalculatorClient
-                        .GetAmountInBaseAsync(item.AssetId, (double)item.Volume, "EUR")
-                        .GetAwaiter().GetResult();
-                }
-
-                if (amount == 0)
-                    continue;
-
-                var operationType = new[] {"EUR", "USD", "CHF", "GBP"}.Contains(item.AssetId)
-                    ? "CardCashIn"
-                    : "CryptoCashIn";
-
-                result += amount;
-
-                deposits.Add(new DepositOperationEntity
-                {
-                    PartitionKey = item.WalletId.ToString(),
-                    RowKey = item.Id.ToString(),
-                    ClientId = item.WalletId.ToString(),
-                    OperationId = item.Id.ToString(),
-                    Asset = item.AssetId,
-                    Amount = (double)item.Volume,
-                    BaseAsset = "EUR",
-                    BaseVolume = (double)amount,
-                    OperationType = operationType,
-                    Date = item.Timestamp
-                });
-            }
-
-            await depositsStorage.InsertOrMergeBatchAsync(deposits);
-
-            return result;
         }
 
         private static IContainer BuildContainer(AppSettings settings)
